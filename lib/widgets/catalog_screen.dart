e8cc05786cf2f5f4edcc04e10110866d55c00b48
Robert Licata <licatar@clarkson.edu>
2023-03-05 06:18:07 -0500

    Redesigned Catalog Screen. Will refactor tonight.

--- lib/widgets/catalog_screen.dart
+++ lib/widgets/catalog_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:majorizer_ui/widgets/main_app_bar.dart';
+import 'package:majorizer_ui/widgets/size_config.dart';
 
 class CatalogScreen extends StatelessWidget {
   const CatalogScreen({super.key});
@@ -8,11 +9,6 @@ class CatalogScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
 
-    Widget title = const Padding(
-      padding: EdgeInsets.fromLTRB(100, 20, 100, 20),
-      child: Text('Course Catalog'),
-    );
-
     return Scaffold(
       appBar: PreferredSize(
         preferredSize: const Size.fromHeight(56),
@@ -20,116 +16,144 @@ class CatalogScreen extends StatelessWidget {
       ),
       body: Column(
         children: [
-          title,
-          CourseDatabase(),
+          TitleBar(),
+          Container(
+            alignment: Alignment.topCenter,
+            height: MediaQuery.of(context).size.height * 0.5,
+            child: ProgramList(
+              items: [
+                ProgramListItem('CS', 'Computer\nScience'),
+                ProgramListItem('CM', 'Chemistry'),
+                ProgramListItem('PY', 'Psychology'),
+                ProgramListItem('MA', 'Mathematics'),
+              ],
+            ),
+          ),
         ],
       ),
     );
   }
 }
 
-class CourseDatabase extends StatelessWidget {
-  CourseDatabase({super.key});
-  final List<String> _tempCourseIDs = [
-    'CS 141',
-    'CS 142',
-    'CS 241',
-    'CS 242',
-    'CS 341',
-    'CS 344',
-  ];
-  final List<String> _tempCredits = [
-    '4',
-    '3',
-    '3',
-    '3',
-    '3',
-    '3',
-  ];
-  final List<String> _tempSemestersOffered = [
-    'Every Semester',
-    'Every Semester',
-    'Spring Only',
-    'Fall Only',
-    'Fall Only',
-    'Spring Only',
-  ];
-  final List<String> _tempNames = [
-    'Introduction to Computer Science I',
-    'Introduction to Computer Science II',
-    'Computer Organization',
-    'Advanced Programming Concepts In Java',
-    'Programming Languages',
-    'Algorithms and Data Structures',
-  ];
-  final List<String> _tempDescriptions = [
-    'This course is an introduction to basic concepts of computer science, with emphasis on programming. Computer programming is to the study of computer science what writing is to the study of literature. It is a primary tool for implementing algorithms in computer science. Fundamental techniques for software design and implementation will be covered and these concepts demonstrated in a programming language like C++. Additional topics include top-down modular design, developing general-purpose software tools, procedural and data abstraction, algorithms, and an introduction to recursion and dynamic data structures. The course consists of three hours of lecture and a one hour computer laboratory session per week.',
-    'This course will further develop and expand upon the topics introduced in CS 141. Advanced programming techniques will be covered, with extensive use of recursion and dynamic data structures. Abstract data types, including lists, queues, trees and graphs, will be studied. Specific emphasis will be given to tree traversals and binary search trees. Algorithms for searching and sorting will be explored along with methods of comparative analysis. The topics in this course provide an essential foundation for the further study of computer science.',
-    'An introduction to computer organization and assembly language programming. Topics include the functional organization of computer hardware; data representation, and computer arithmetic; instruction sets, addressing modes and low-level I/O. Introduces machine and assembly language, and systems programming techniques in the programming language C. This course serves as a foundation for courses on operating systems, compilers, networks, and computer artchitecture.',
-    'This course builds upon the foundation topics covered in CS 142, and covers concepts and skills required for real-life, modern programming. Topics will include basic object-oriented programming design, graphical user interfaces (GUIs), exception handling, multithreading and synchronization, networking, and client/server applications. The programming language Java with its companion OOP/GUI libraries will be used to illustrate these topics. This course will emphasize team programming on a large-scale project with a realistic deadline.',
-    'This course examines the major paradigms underlying modern programming languages. The course currently focuses on object-oriented and logic programming. The rationale of the paradigms is discussed along with typical programming idioms used with them. Programming exercises are used to illustrate concepts--the course does not aim to make students proficient programmers in all the languages that will be studied.',
-    'The primary goal of this course is to build on the programming skills gained in CS 141 and 142 to introduce students to more sophisticated algorithms and data structures and the notion of algorithm design. The course also introduces the basic formalism and concepts used in the analysis of algorithms. The relative efficiency of the algorithms studied is estimated by informal application of these ideas. The algorithms and data structures discussed include those for sorting and searching, pattern matching, set representation, graph problems, dynamic programming and others. Programming exercises based on \'realistic\' applications help students to understand the often difficult process of reducing a real-world problem to a standard algorithmic question.',
-  ];
-  // TODO: Implement... final List<String> _tempCommPoints = [];
-  final List<String> _tempDesignation = [
-    'CS',
-    'CS',
-    'CS',
-    'CS',
-    'CS',
-    'CS',
-  ];
-  final List<String> _tempInstructorID = [''];
-
-  final List<DataColumn> _columns = const [
-    DataColumn(
-      label: Expanded(
-        child: Text(
-          'Course ID',
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
-      ),
-    ),
-    DataColumn(
-      label: Expanded(
-        child: Text(
-          'Course',
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
-      ),
-    ),
-    DataColumn(
-      label: Expanded(
-        child: Text(
-          'Credits',
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
-      ),
-    ),
-    DataColumn(
-      label: Expanded(
-        child: Text(
-          'Semesters Offered',
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
-      ),
-    ),
-  ];
+class TitleBar extends StatelessWidget {
+  const TitleBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return DataTable(
-      columns: _columns,
-      rows: const <DataRow>[
-        DataRow(
-          cells: <DataCell>[
-            DataCell(Text('Intro to Software Design and Development')),
-            DataCell(Text('CS 141')),
-            DataCell(Text('CS 141')),
-            DataCell(Text('CS 141')),
-          ],
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 30),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            'Course Catalog',
+            style: TextStyle(
+              color: Color(0xFFda6237),
+              decoration: TextDecoration.none,
+              fontSize: 50,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Spacer(),
+          Flexible(child: search(context)),
+        ],
+      ),
+    );
+  }
+
+  Widget search(BuildContext context) {
+    return Container(
+      constraints: BoxConstraints(maxWidth: 400, maxHeight: 100),
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: 'Search Courses',
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(width: 3, color: Color(0xFFda6237)),
+          ),
+          suffixIcon: Icon(
+            Icons.search,
+            color: Color(0xFFda6237),
+          ),
+          focusColor: Color(0xFFda6237),
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(width: 3, color: Colors.grey),
+          ),
         ),
-      ],
+      ),
+    );
+  }
+}
+
+class ProgramList extends StatelessWidget {
+  final List<ProgramListItem> items;
+  const ProgramList({super.key, required this.items});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      shrinkWrap: true,
+      padding: const EdgeInsets.all(20),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 5,
+        mainAxisSpacing: 20,
+        crossAxisSpacing: 20,
+      ),
+      children: items,
+    );
+  }
+}
+
+class ProgramListItem extends StatelessWidget {
+  String programID;
+  String programName;
+
+  ProgramListItem(this.programID, this.programName);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        foregroundColor: Colors.black,
+        backgroundColor: Colors.white,
+      ),
+      child: Column(
+        children: [
+          buildID(context),
+          buildName(context),
+        ],
+      ),
+    );
+  }
+
+  Widget buildID(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.fromLTRB(0, 30, 0, 10),
+      padding: const EdgeInsets.all(5),
+      child: Text(
+        programID,
+        style: TextStyle(
+          decoration: TextDecoration.none,
+          fontSize: MediaQuery.of(context).size.height * 0.1,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  Widget buildName(BuildContext context) {
+    return Text(
+      programName,
+      textAlign: TextAlign.center,
+      overflow: TextOverflow.ellipsis,
+      style: TextStyle(
+        decoration: TextDecoration.none,
+        fontSize: MediaQuery.of(context).size.height * 0.04,
+        fontWeight: FontWeight.bold,
+      ),
     );
   }
 }
